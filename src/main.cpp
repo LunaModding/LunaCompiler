eaaf1cbe1cfb445c80bb8c16d1e95fd642aec9f1
Millzy <millzyyt08@gmail.com>
2023-01-22 13:07:07 +0000

    this shit

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
-#include "main.hpp"
-#include <iostream>
+#include "CLI/App.hpp"
+
+#define SUCCESS return 0
 
 int main() {
-    std::cout << str << std::endl;
-    return 0;
+    CLI::App app{APP_DESCRIPTION, APP_NAME};
+
+    auto *init = app.add_subcommand("init", "Creates a new Luna project in the current directory.");
+    init->callback();
+
+    app.require_subcommand();
+
+    CLI11_PARSE(app)
+    SUCCESS;
 }
