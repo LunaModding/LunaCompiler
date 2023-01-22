8e9b4c72245449f1bc53d3f7b816b1f19f97b4bf
Millzy <millzyyt08@gmail.com>
2023-01-22 13:07:15 +0000

    init command base

---
+++ include/init_command.hpp
@@ -0,0 +1,4 @@
+#include <functional>
+#include "CLI/App.hpp"
+
+std::function<void()> init_callback(CLI::App &app);
---
+++ src/init_command.cpp
@@ -0,0 +1,5 @@
+#include "init_command.hpp"
+
+std::function<void()> init_callback(CLI::App &app) {
+
+}
