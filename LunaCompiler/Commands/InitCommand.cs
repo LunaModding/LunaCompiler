037d58f1592a058845401382ea7629686bfd55b7
Millzy <millzyyt08@gmail.com>
2023-01-23 20:55:12 +0000

    example command

---
+++ LunaCompiler/Commands/InitCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.CommandLine;
+
+namespace LunaCompiler.Commands
+{
+    public class InitCommand : CommandBase
+    {
+        public override string Name => "init";
+
+        public override Option<object>[] Options => Array.Empty<Option<object>>();
+
+        public override Argument<object>[] Arguments => Array.Empty<Argument<object>>();
+
+        public override Command[] SubCommands => Array.Empty<Command>();
+
+        public void Handler()
+        {
+            Console.WriteLine("Nice init bro.");
+        }
+
+        protected override Command Build(Command command)
+        {
+            command.SetHandler(Handler);
+            return command;
+        }
+    }
+}
