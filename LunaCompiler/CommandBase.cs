6b588db39afda62ef918b0b1e9dbed966b038395
Millzy <millzyyt08@gmail.com>
2023-01-23 20:55:06 +0000

    this

---
+++ LunaCompiler/CommandBase.cs
@@ -0,0 +1,19 @@
+﻿using System.CommandLine;
+
+namespace LunaCompiler
+{
+    public abstract class CommandBase
+    {
+        public abstract string Name { get; }
+        public string? Description { get; } = null;
+        public abstract Option<object>[] Options { get; }
+        public abstract Argument<object>[] Arguments { get; }
+        public abstract Command[] SubCommands { get; }
+        protected abstract Command Build(Command command);
+
+        public Command Build()
+        {
+            return Build(new Command(Name, Description));
+        }
+    }
+}
---
+++ LunaCompiler/Program.cs
@@ -0,0 +1,27 @@
+﻿using LunaCompiler.Commands;
+using System.CommandLine;
+
+namespace LunaCompiler
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            var rootCommand = new RootCommand();
+            rootCommand.AddCommand(new InitCommand().Build());
+
+            rootCommand.SetHandler(HandleRoot);
+
+            return await rootCommand.InvokeAsync(args);
+        }
+
+        private static void HandleRoot()
+        {
+            Console.WriteLine("Luna Compiler - Create and compile Luna projects.");
+            Console.WriteLine();
+            Console.WriteLine("-- Commands --");
+            Console.WriteLine("init - Creates a new Luna project.");
+            Console.WriteLine("build - Compiles the Luna project in the current directory to a .lunad file.");
+        }
+    }
+}
