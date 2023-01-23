26d8f5f31abcf1aae22f6991ec59b57eb112ea2a
Millzy <millzyyt08@gmail.com>
2023-01-23 17:16:35 +0000

    I give up, back to C++

--- build.ps1
+++
@@ -1,9 +0,0 @@
-if (!(Get-Module -ListAvailable -Name ps2exe)) {
-    Install-Module ps2exe
-}
-
-if (!(Test-Path -Path ".\build\")) {
-    New-Item -Path ".\build\" -ItemType Directory
-}
-
-ps2exe .\lunac.ps1 .\build\lunac.exe -verbose
