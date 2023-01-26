ab42095b50179060835d2bf13a9c1ab284a1da37
Millzy <millzyyt08@gmail.com>
2023-01-26 21:56:31 +0000

    base compile command

---
+++ LunaCompiler/Commands/CompileCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunaCompiler.Commands
+{
+    public class CompileCommand : Command
+    {
+        public CompileCommand() : base("compile", "Compiles a Luna project into a .lunad file.")
+        {
+            AddAlias("c");
+
+            var compiler = new Option<string>(new string[] { "--compiler", "-c" })
+            {
+                Name = "compiler",
+                Description = "The path to the compiler to use to compile .lua source files before they are packaged into a .lunad file.",
+                IsRequired = false,
+            };
+
+            var arguments = new Option<string>(new string[] { "--arguments", "-a" })
+            {
+                Name = "arguments",
+                Description = "The arguments to use if a compiler is set to compile .lua source files before they are packaged into a .lunad file.",
+                IsRequired = false,
+            };
+
+            AddOption(compiler);
+            AddOption(arguments);
+
+            this.SetHandler(HandleCommand, compiler, arguments);
+        }
+
+        private void HandleCommand(string compiler, string arguments)
+        {
+
+        }
+    }
+}
